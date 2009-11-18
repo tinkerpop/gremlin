e274f1b90780ae6dabc69051731c9147490ccc4c
marko <marko@nenofhat.local>
2009-11-17 18:00:51 -0700

    added stubs for the Sesame implementation of the General Graph API.

---
+++ trunk/src/main/java/com/tinkerpop/gremlin/db/neo/NeoGraph.java
@@ -0,0 +1,8 @@
+package com.tinkerpop.gremlin.db.neo;
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ * @version 0.1
+ */
+public class NeoGraph {
+}
---
+++ trunk/src/main/java/com/tinkerpop/gremlin/db/sesame/SesameVertex.java
@@ -0,0 +1,8 @@
+package com.tinkerpop.gremlin.db.sesame;
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ * @version 0.1
+ */
+public class SesameVertex {
+}
