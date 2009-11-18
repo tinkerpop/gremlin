7bdd8d73f16b06fae9526b7580b333146d8d521f
marko <marko@252.sub-70-212-254.myvzw.com>
2009-11-18 11:53:02 -0700

    sesame enviorment is all ready to go. I was in MVN hell for a bit with dependcies. It sucked. Have SesameGraphTest up and there is some static methods for creating a MemoryStore with a provided RDF file loaded into it. After lunch I will deal with testing Gremlin over this mMemoryStore.

--- trunk/src/test/java/com/tinkerpop/gremlin/db/sesame/SesameGraphTest.java
+++ trunk/src/test/java/com/tinkerpop/gremlin/db/sesame/SesameGraphTest.java
@@ -7,6 +7,10 @@ import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.sail.memory.MemoryStore;
 import org.openrdf.sail.SailConnection;
+import org.openrdf.sail.SailException;
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.model.Statement;
+import info.aduna.iteration.CloseableIteration;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
@@ -14,18 +18,34 @@ import org.openrdf.sail.SailConnection;
  */
 public class SesameGraphTest extends TestCase {
 
-    public void testLoadTinkerGraphExample() throws Exception {
+    public static MemoryStore loadMemoryStore(String tripleFile, RDFFormat tripleFileFormat) throws Exception {
         MemoryStore sail = new MemoryStore();
         sail.initialize();
         Repository repo = new SailRepository(sail);
         RepositoryConnection connection = repo.getConnection();
-        connection.add(SesameGraph.class.getResourceAsStream("graph-example-1.ntriple"), null, RDFFormat.NTRIPLES);
+        connection.add(SesameGraph.class.getResourceAsStream(tripleFile), new String(), tripleFileFormat, new URIImpl("http://tinkerpop.com#graph"));
         connection.commit();
         connection.close();
+        return sail;
+    }
+
+    public void testGraphExample1() throws Exception {
+        MemoryStore sail = loadMemoryStore("graph-example-1.ntriple", RDFFormat.NTRIPLES);
         SailConnection sc = sail.getConnection();
-        assertTrue(sc.getStatements(null,null,null,false).hasNext());
+        CloseableIteration<? extends Statement, SailException> results = sc.getStatements(null,null,null,false);
+        assertEquals(6, countStatements(results, true));
         sail.shutDown();
+    }
 
-
+    public static int countStatements(CloseableIteration<? extends Statement, SailException> itty, boolean print) throws SailException {
+        int counter = 0;
+        while(itty.hasNext()) {
+            Statement s = itty.next();
+            if(print)
+                System.out.println(s);
+            counter++;
+        }
+        itty.close();
+        return counter;
     }
 }
