6704adad80875b9b9ad3dcf08407bd274ef225e9
marko <okrammarko@gmail.com>
2010-02-05 15:53:35 -0700

    started the first incarnation of jung algorithm connects to gremlin... this is going to be a sweet function library. note, none of this has been tested so far---test cases needed.

---
+++ src/main/java/com/tinkerpop/gremlin/models/ggm/jung/JungFunctions.java
@@ -0,0 +1,7 @@
+package com.tinkerpop.gremlin.models.ggm.jung;
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class JungFunctions {
+}
---
+++ src/main/java/com/tinkerpop/gremlin/models/ggm/jung/functions/scoring/PageRankFunction.java
@@ -0,0 +1,9 @@
+package com.tinkerpop.gremlin.models.ggm.jung.functions.scoring;
+
+import org.apache.commons.jxpath.Function;
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class PageRankFunction implements Function {
+}
