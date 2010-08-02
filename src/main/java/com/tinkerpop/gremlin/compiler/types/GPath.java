6b3ea8a7730630fc746c1f639f63c2b3e754d681
Pavel Yaskevich <povel.y@gmail.com>
2010-08-03 01:35:08 +0300

    GPath implements Comparable and overrides equals(Object o) method - can be used in logic operations e.g. (./outE/inV[0]/@age) >= 27

--- src/main/java/com/tinkerpop/gremlin/compiler/types/GPath.java
+++ src/main/java/com/tinkerpop/gremlin/compiler/types/GPath.java
@@ -7,13 +7,14 @@ import com.tinkerpop.gremlin.compiler.pipes.GremlinRangeFilterPipe;
 import com.tinkerpop.pipes.Pipe;
 import com.tinkerpop.pipes.Pipeline;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 /**
  * @author Pavel A. Yaskevich
  */
-public class GPath implements Iterable {
+public class GPath implements Iterable, Comparable {
 
     private final List<Pipe> pipes;
     private final Atom<Object> root;
@@ -41,5 +42,43 @@ public class GPath implements Iterable {
         pipeline.setStarts(GremlinPipesHelper.pipelineStartPoint(((persistentRoot == null) ? root.getValue() : persistentRoot)));
         return pipeline;
     }
-    
+
+    public List<Object> toList() {
+        Iterator current = this.iterator();
+        List<Object> results = new ArrayList<Object>();
+        
+        while(current.hasNext()) {
+            results.add(current.next());    
+        }
+
+        return results;
+    }
+
+    public int compareTo(Object o) {
+        List<Object> list = this.toList();
+
+        if (list.size() == 1) {
+            Object c = list.get(0);
+            if (c instanceof Comparable && o instanceof Comparable)
+                return ((Comparable<Object>) c).compareTo(o);
+        }
+
+        throw new ClassCastException(this + " and " + o + " are incomparable objects");
+    }
+
+    public boolean equals(Object o) {
+        return this.toList().equals(convertToList(o));
+    }
+
+    protected Iterable convertToList(Object operand) {
+        if (operand instanceof GPath) {
+            return ((GPath) operand).toList();
+        } else if (operand instanceof Iterable) {
+            return (Iterable) operand;
+        } else {
+            List<Object> list = new ArrayList<Object>();
+            list.add(operand);
+            return list;
+        }
+    }
 }
