ccf2d191796b54c0b9eee1808e04effd2d5b3239
Pavel Yaskevich <povel.y@gmail.com>
2010-08-14 23:50:05 +0300

    fix for issue/70 - Allow for Gpath assignment using := notation (related to issue 60).

--- src/main/java/com/tinkerpop/gremlin/compiler/operations/util/DeclareVariable.java
+++ src/main/java/com/tinkerpop/gremlin/compiler/operations/util/DeclareVariable.java
@@ -1,9 +1,11 @@
 package com.tinkerpop.gremlin.compiler.operations.util;
 
+import com.tinkerpop.gremlin.compiler.Tokens;
 import com.tinkerpop.gremlin.compiler.context.GremlinScriptContext;
 import com.tinkerpop.gremlin.compiler.operations.Operation;
 import com.tinkerpop.gremlin.compiler.types.Atom;
 import com.tinkerpop.gremlin.compiler.types.DynamicEntity;
+import com.tinkerpop.gremlin.compiler.types.Var;
 
 /**
  * @author Pavel A. Yaskevich
@@ -13,7 +15,7 @@ public class DeclareVariable implements Operation {
     private final String name;
     private final Operation valueOperation;
     private final GremlinScriptContext context;
-    
+
     public DeclareVariable(final String name, final Operation valueOp, final GremlinScriptContext context) {
         this.name = name;
         this.valueOperation = valueOp;
@@ -22,14 +24,24 @@ public class DeclareVariable implements Operation {
 
     public Atom compute() {
         final Atom atom = this.valueOperation.compute();
-        final Atom value = (!(atom instanceof DynamicEntity)) ? atom : new Atom<Object>(atom.getValue());
-        
+        final Atom value = (!(atom instanceof DynamicEntity)) ? atom : dynamicValue(atom);
+
         this.context.getVariableLibrary().declare(this.name, value);
         return atom;
     }
-    
+
     public Type getType() {
         return Type.STATEMENT;
     }
 
+    private Atom<Object> dynamicValue(final Atom atom) {
+        if (atom instanceof Var) {
+            final Var variable = (Var) atom;
+            if (variable.getVariableName().equals(Tokens.IDENTITY)) {
+                return new Atom<Object>(this.context.getCurrentPoint());
+            }
+        }
+
+        return new Atom<Object>(atom.getValue());
+    }
 }
