b803b363382d1168a5d59ecdc18736c87d97c2b2
Pavel Yaskevich <povel.y@gmail.com>
2010-09-22 18:34:27 +0300

    fixed gremlin.bat to work with command line arguments. Thanks to http://github.com/hazard.

--- gremlin.bat
+++ gremlin.bat
@@ -1,9 +1,33 @@
 :: Windows launcher script for Gremlin
+@echo off
 
-if not "%JAVA_OPTIONS%" == "" goto gotOpts
 set JAVA_OPTIONS=-Xms32M -Xmx512M
-:gotOpts
 
+:: Launch the application
+if "%1" == "" goto console
+if "%1" == "-e" goto script
+if "%1" == "-v" goto version
+
+:console
 java %JAVA_OPTIONS% %JAVA_ARGS% -cp target/gremlin-*-standalone.jar com.tinkerpop.gremlin.Console
+goto :eof
 
-:: TODO: return exit code
+:script
+
+set strg=
+
+FOR %%X IN (%*) DO (
+CALL :concat %%X %1 %2
+)
+
+java %JAVA_OPTIONS% %JAVA_ARGS% -cp target/gremlin-*-standalone.jar com.tinkerpop.gremlin.ScriptExecutor %strg%
+goto :eof
+
+:version
+echo Gremlin 0.5
+goto :eof
+
+:concat
+if %1 == %2 goto skip
+SET strg=%strg% %1
+:skip
