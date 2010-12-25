8a5d758ac0e3bbbc986c3dc7e81d23ecc270f870
okram <okrammarko@gmail.com>
2010-12-25 01:39:13 -0700

    lots more work on GroovyGremlin. I'm amazed at how little code produces so many results in Groovy. Its insane.

--- src/main/groovy/com/tinkerpop/gremlin/groovy/GroovyGremlin.groovy
+++ src/main/groovy/com/tinkerpop/gremlin/groovy/GroovyGremlin.groovy
@@ -1,28 +1,81 @@
 package com.tinkerpop.gremlin.groovy;
 
+
+import com.tinkerpop.blueprints.pgm.Edge
 import com.tinkerpop.blueprints.pgm.Element
+import com.tinkerpop.blueprints.pgm.Graph
+import com.tinkerpop.pipes.IdentityPipe
 import com.tinkerpop.pipes.Pipe
-import com.tinkerpop.pipes.pgm.EdgeVertexPipe
-import com.tinkerpop.pipes.pgm.GraphElementPipe
-import com.tinkerpop.pipes.pgm.IdPipe
-import com.tinkerpop.pipes.pgm.VertexEdgePipe
+import com.tinkerpop.pipes.filter.ComparisonFilterPipe.Filter
+import com.tinkerpop.pipes.filter.RangeFilterPipe
+import com.tinkerpop.pipes.pgm.*
+import com.tinkerpop.pipes.AbstractPipe
 
 class GroovyGremlin {
   static {
-    Set tokens = ["outE", "inE", "outV", "inV", "V", "E"] as Set;
 
-    Object.metaClass.propertyMissing = {final String name ->
-      if (delegate instanceof Element && !tokens.contains(name)) {
-        return delegate.getProperty(name)
+    final String ID = "id";
+    final String LABEL = "label";
+    final Set tokens = ["outE", "inE", "outV", "inV", "V", "E", "i"] as Set;
+
+    /*Object.metaClass.propertyMissing = {final String name ->
+      return delegate."$name"();
+    }*/
+
+    Pipe.metaClass.propertyMissing = {final String name ->
+      if (!tokens.contains(name)) {
+        if (name.equals(ID)) {
+          Pipe pipe = new IdPipe();
+          pipe.setStarts(delegate);
+          return pipe;
+        } else if (name.equals(LABEL)) {
+          Pipe pipe = new LabelPipe();
+          pipe.setStarts(delegate);
+          return pipe;
+        } else {
+          Pipe pipe = new PropertyPipe(name)
+          pipe.setStarts(delegate);
+          return pipe;
+        }
       } else {
         return delegate."$name"();
       }
     }
 
-    def compose = {final Object start, final Pipe pipe, final Closure c ->
+    Iterator.metaClass.getAt = {final Range range ->
+      Pipe pipe = new RangeFilterPipe(range.getFrom(), range.getTo());
+      pipe.setStarts(delegate);
+      return pipe;
+    }
+
+    VertexEdgePipe.metaClass.getAt = {final String label ->
+      if (label.startsWith(":")) {
+        Pipe pipe = new LabelFilterPipe(label.substring(1), Filter.NOT_EQUAL);
+        pipe.setStarts(delegate);
+        return pipe;
+      } else {
+        return delegate.propertyMissing(label);
+      }
+    }
+
+    Element.metaClass.propertyMissing = {final String name ->
+      if (!tokens.contains(name)) {
+        if (name.equals(ID)) {
+          return delegate.getId();
+        } else if (delegate instanceof Edge && name.equals(LABEL)) {
+          return delegate.getLabel();
+        } else {
+          return delegate.getProperty(name)
+        }
+      } else {
+        return delegate."$name"();
+      }
+    }
+
+    def compose = {final Object start, final Pipe pipe, final Closure closure ->
       pipe.setStarts(start.iterator());
-      if (c) {
-        final Pipe pipe2 = new ClosureFilterPipe(c);
+      if (closure) {
+        final Pipe pipe2 = new ClosureFilterPipe(closure);
         pipe2.setStarts(pipe);
         return pipe2;
       } else {
@@ -30,54 +83,44 @@ class GroovyGremlin {
       }
     }
 
-    Object.metaClass.outE = {final Closure c ->
-      return compose.call(delegate, new VertexEdgePipe(VertexEdgePipe.Step.OUT_EDGES), c)
+    Pipe.metaClass.call = {final Closure closure ->
+      return compose.call(delegate, new IdentityPipe(), closure)
     }
 
-    Object.metaClass.inE = {final Closure c ->
-      return compose.call(delegate, new VertexEdgePipe(VertexEdgePipe.Step.IN_EDGES), c)
+    Object.metaClass.i = {final Closure closure ->
+      return compose.call(delegate, new IdentityPipe(), closure)
     }
 
-    Object.metaClass.inV = {final Closure c ->
-      return compose.call(delegate, new EdgeVertexPipe(EdgeVertexPipe.Step.IN_VERTEX), c)
+    Object.metaClass.outE = {final Closure closure ->
+      return compose.call(delegate, new VertexEdgePipe(VertexEdgePipe.Step.OUT_EDGES), closure)
     }
 
-    Object.metaClass.outV = {final Closure c ->
-      return compose.call(delegate, new EdgeVertexPipe(EdgeVertexPipe.Step.OUT_VERTEX), c)
+    Object.metaClass.inE = {final Closure closure ->
+      return compose.call(delegate, new VertexEdgePipe(VertexEdgePipe.Step.IN_EDGES), closure)
     }
 
-    Object.metaClass.V = {final Closure c ->
-      return compose.call(delegate, new GraphElementPipe(GraphElementPipe.ElementType.VERTEX), c)
+    Object.metaClass.inV = {final Closure closure ->
+      return compose.call(delegate, new EdgeVertexPipe(EdgeVertexPipe.Step.IN_VERTEX), closure)
     }
 
-    Object.metaClass.E = {final Closure c ->
-      return compose.call(delegate, new GraphElementPipe(GraphElementPipe.ElementType.EDGE), c)
+    Object.metaClass.outV = {final Closure closure ->
+      return compose.call(delegate, new EdgeVertexPipe(EdgeVertexPipe.Step.OUT_VERTEX), closure)
     }
 
-    Object.metaClass.id = {final Closure c ->
-      return compose.call(delegate, new IdPipe(), c);
+    Object.metaClass.V = {final Closure closure ->
+      return compose.call(delegate, new GraphElementPipe(GraphElementPipe.ElementType.VERTEX), closure)
     }
 
-    // todo: determine if findAll{} is truly iterator friendly. If so, findAll{} is !filter{}
-    Object.metaClass.filter = {final Closure c ->
-      final Pipe pipe = new ClosureFilterPipe(c);
-      pipe.setStarts(delegate.iterator());
-      return pipe;
+    Object.metaClass.E = {final Closure closure ->
+      return compose.call(delegate, new GraphElementPipe(GraphElementPipe.ElementType.EDGE), closure)
     }
 
-    // todo: determine if each{} is truly iterator friendly. If so, pipe{} is each{}
-    Object.metaClass.pipe = {final Closure c ->
-      final Pipe pipe = new ClosurePipe(c);
-      pipe.setStarts(delegate.iterator());
-      return pipe;
+    Graph.metaClass.v = {final Object id ->
+      return delegate.getVertex(id);
     }
 
-    // todo: determine if each{} is truly iterator friendly. if so, eval{} is each{} with no closure
-    Object.metaClass.eval() {
-      final Iterator itty = delegate.iterator();
-      while (itty.hasNext()) {
-        itty.next();
-      }
+    Graph.metaClass.e = {final Object id ->
+      return delegate.getEdge(id);
     }
   }
 }
--- src/test/groovy/com/tinkerpop/gremlin/groovy/GroovyGremlinTest.groovy
+++ src/test/groovy/com/tinkerpop/gremlin/groovy/GroovyGremlinTest.groovy
@@ -1,15 +1,16 @@
 package com.tinkerpop.gremlin.groovy
 
 import junit.framework.TestCase
+import com.tinkerpop.blueprints.pgm.impls.tg.TinkerGraphFactory
+import com.tinkerpop.blueprints.pgm.Graph
 
 class GroovyGremlinTest extends TestCase {
 
   public void testGremlinGroovy() {
-    /*new GroovyGremlin();
+    new GroovyGremlin();
     Graph g = TinkerGraphFactory.createTinkerGraph();
-    g.getVertex(1).outE.inV.each {println it}
-    g.getVertex(1).outE {it.label == 'knows'}.inV.each {println it.name}
-    g.V.outE.each {println it}*/
-    assertTrue(true);
+
+
+    g.v(1).outE['weight'].each{println it}
   }
 }
