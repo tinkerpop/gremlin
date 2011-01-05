60fd5d533f16945c80d87f20f1655dd1212e8014
okram <okrammarko@gmail.com>
2011-01-05 13:51:54 -0700

    fixed a bug in GremlinScriptEngine. Gremlin() wasn't being loaded at construction. Added a test case to validate behavior.

--- src/main/java/com/tinkerpop/gremlin/jsr223/GremlinScriptEngine.java
+++ src/main/java/com/tinkerpop/gremlin/jsr223/GremlinScriptEngine.java
@@ -1,10 +1,15 @@
 package com.tinkerpop.gremlin.jsr223;
 
+import com.tinkerpop.gremlin.Gremlin;
 import org.codehaus.groovy.jsr223.GroovyScriptEngineImpl;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
 public class GremlinScriptEngine extends GroovyScriptEngineImpl {
+
+    public GremlinScriptEngine() {
+        new Gremlin();
+    }
 
 }
---
+++ src/test/java/com/tinkerpop/gremlin/jsr223/GremlinScriptEngineTest.java
@@ -0,0 +1,24 @@
+package com.tinkerpop.gremlin.jsr223;
+
+import com.tinkerpop.blueprints.pgm.impls.tg.TinkerGraphFactory;
+import com.tinkerpop.gremlin.BaseTest;
+
+import javax.script.ScriptEngine;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class GremlinScriptEngineTest extends BaseTest {
+
+    public void testGremlinLoading() throws Exception {
+        ScriptEngine engine = new GremlinScriptEngine();
+        List list = new ArrayList();
+        engine.put("g", TinkerGraphFactory.createTinkerGraph());
+        engine.put("list", list);
+        assertEquals(list.size(), 0);
+        engine.eval("g.v(1).outE.inV >> list");
+        assertEquals(list.size(), 3);
+    }
+}
