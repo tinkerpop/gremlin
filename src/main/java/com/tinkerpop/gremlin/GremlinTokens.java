db08fae6264866b6f015e559d6aeab3920c5b796
okram <okrammarko@gmail.com>
2011-01-22 14:20:45 -0700

    Gremlin 0.7 Gremopoly release

--- src/main/java/com/tinkerpop/gremlin/GremlinTokens.java
+++ src/main/java/com/tinkerpop/gremlin/GremlinTokens.java
@@ -5,7 +5,7 @@ package com.tinkerpop.gremlin;
  */
 public class GremlinTokens {
 
-    public static final String VERSION = "0.7-SNAPSHOT";
+    public static final String VERSION = "0.7";
     public static final String LABEL = "label";
     public static final String ID = "id";
 
