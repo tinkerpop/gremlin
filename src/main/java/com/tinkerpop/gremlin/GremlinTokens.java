3017e6c18e3a65c6528917f26959e9ad92cdcf7e
okram <okrammarko@gmail.com>
2011-03-02 09:46:16 -0700

    Gremlin 0.8 Grem Stefani release commit.

--- src/main/java/com/tinkerpop/gremlin/GremlinTokens.java
+++ src/main/java/com/tinkerpop/gremlin/GremlinTokens.java
@@ -5,7 +5,7 @@ package com.tinkerpop.gremlin;
  */
 public class GremlinTokens {
 
-    public static final String VERSION = "0.8-SNAPSHOT";
+    public static final String VERSION = "0.8";
     public static final String LABEL = "label";
     public static final String ID = "id";
 
