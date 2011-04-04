999d1db2d5db482d35c418985ef084791ad64bfd
Marko Rodriguez <okrammarko@gmail.com>
2011-04-04 13:21:40 -0600

    bumped to 1.0-SNAPSHOT.

--- src/main/java/com/tinkerpop/gremlin/GremlinTokens.java
+++ src/main/java/com/tinkerpop/gremlin/GremlinTokens.java
@@ -5,7 +5,7 @@ package com.tinkerpop.gremlin;
  */
 public class GremlinTokens {
 
-    public static final String VERSION = "0.9";
+    public static final String VERSION = "1.0-SNAPSHOT";
     public static final String LABEL = "label";
     public static final String ID = "id";
 
