71502037eadac079c2145acd8664f3cd94b9f19a
Marko Rodriguez <okrammarko@gmail.com>
2011-05-08 17:07:24 -0600

    final doc tweaks for 1.0 release.

--- src/main/java/com/tinkerpop/gremlin/GremlinTokens.java
+++ src/main/java/com/tinkerpop/gremlin/GremlinTokens.java
@@ -5,7 +5,7 @@ package com.tinkerpop.gremlin;
  */
 public class GremlinTokens {
 
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.0";
     public static final String LABEL = "label";
     public static final String ID = "id";
 
