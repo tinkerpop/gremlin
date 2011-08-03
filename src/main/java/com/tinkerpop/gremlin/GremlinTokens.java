f973762c531dcdf9aefaf0506aa2873b81feefb4
Marko Rodriguez <okrammarko@gmail.com>
2011-08-03 08:46:42 -0600

    bumped to Gremlin 1.3-SNAPSHOT.

--- src/main/java/com/tinkerpop/gremlin/GremlinTokens.java
+++ src/main/java/com/tinkerpop/gremlin/GremlinTokens.java
@@ -5,7 +5,7 @@ package com.tinkerpop.gremlin;
  */
 public class GremlinTokens {
 
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3-SNAPSHOT";
     public static final String LABEL = "label";
     public static final String ID = "id";
 
