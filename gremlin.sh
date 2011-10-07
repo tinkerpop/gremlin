56cb1e5150998e074c1f8f68e300503c763f89ef
Marko Rodriguez <okrammarko@gmail.com>
2011-10-06 21:09:57 -0600

    reworked gremlin.sh to support gremlin.sh -l scala or groovy.

--- gremlin.sh
+++ gremlin.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-`dirname $0`/gremlin-groovy/target/gremlin-*-standalone/bin/gremlin.sh $@
+# Launch the application
+if [ "$1" = "-l" ]; then
+  if [ "$2" = "groovy" ]; then
+    `dirname $0`/gremlin-groovy/target/gremlin-*-standalone/bin/gremlin.sh $@
+  elif [ "$2" = "scala" ]; then
+    `dirname $0`/gremlin-scala/target/gremlin-*-standalone/bin/gremlin.sh $@
+  else
+    echo "The only Gremlin REPL JVM languages are 'groovy' and 'scala'"
+  fi
+else
+  `dirname $0`/gremlin-groovy/target/gremlin-*-standalone/bin/gremlin.sh $@
+fi
