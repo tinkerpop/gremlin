5feb5f2ecdb500dfffc9deabf99c4f43a13932eb
Marko Rodriguez <okrammarko@gmail.com>
2011-10-18 12:19:20 -0600

    added PathStepTest to test path functions, looping and paths, etc. Also, more tweaks to pom.xmls. Still issue of sibling dependencies not compiling.

---
+++ gremlin-groovy/src/test/groovy/com/tinkerpop/gremlin/groovy/transform/PathStepTest.groovy
@@ -0,0 +1,33 @@
+package com.tinkerpop.gremlin.groovy.transform
+
+import com.tinkerpop.blueprints.pgm.Graph
+import com.tinkerpop.blueprints.pgm.impls.tg.TinkerGraphFactory
+import com.tinkerpop.gremlin.Gremlin
+import com.tinkerpop.gremlin.test.UtilitiesTest
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+class PathStepTest extends com.tinkerpop.gremlin.test.transform.PathStepTest {
+
+    Graph g = TinkerGraphFactory.createTinkerGraph();
+    static {
+        Gremlin.load();
+    }
+
+    public void testCompliance() {
+        UtilitiesTest.testCompliance(this.getClass());
+    }
+
+    public void test_g_v1_propertyXnameX_paths() {
+        super.test_g_v1_propertyXnameX_paths(g.v(1)._.name.paths);
+    }
+
+    public void test_g_v1_out_pathsXage__nameX() {
+        super.test_g_v1_out_pathsXage__nameX(g.v(1).out.paths{it.age}{it.name});
+    }
+
+    public void test_g_V_out_loopX1__loops_lt_3X_pathsXit__name__langX() {
+        super.test_g_V_out_loopX1__loops_lt_3X_pathsXit__name__langX(g.V.out.loop(1){it.loops<3}.paths{it}{it.name}{it.lang});
+    }
+}
---
+++ gremlin-test/src/main/java/com/tinkerpop/gremlin/test/transform/PathStepTest.java
@@ -0,0 +1,54 @@
+package com.tinkerpop.gremlin.test.transform;
+
+import com.tinkerpop.blueprints.pgm.Vertex;
+import com.tinkerpop.pipes.Pipe;
+import junit.framework.TestCase;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class PathStepTest extends TestCase {
+
+    public void testCompliance() {
+        assertTrue(true);
+    }
+
+    public void test_g_v1_propertyXnameX_paths(final Pipe<Vertex, List> pipe) {
+        List list = pipe.next();
+        assertFalse(pipe.hasNext());
+        assertEquals(list.size(), 2);
+        assertEquals(((Vertex) list.get(0)).getProperty("name"), "marko");
+        assertEquals(list.get(1), "marko");
+    }
+
+    public void test_g_v1_out_pathsXage__nameX(final Pipe<Vertex, List> pipe) {
+        int counter = 0;
+        Set<String> names = new HashSet<String>();
+        while (pipe.hasNext()) {
+            counter++;
+            List list = pipe.next();
+            assertEquals(list.get(0), 29);
+            assertTrue(list.get(1).equals("josh") || list.get(1).equals("vadas") || list.get(1).equals("lop"));
+            names.add((String) list.get(1));
+        }
+        assertEquals(counter, 3);
+        assertEquals(names.size(), 3);
+    }
+
+    public void test_g_V_out_loopX1__loops_lt_3X_pathsXit__name__langX(final Pipe<Vertex, List> pipe) {
+        int counter = 0;
+        while (pipe.hasNext()) {
+            counter++;
+            List list = pipe.next();
+            assertEquals(list.size(), 3);
+            assertEquals(((Vertex) list.get(0)).getProperty("name"), "marko");
+            assertEquals(list.get(1), "josh");
+            assertEquals(list.get(2), "java");
+        }
+        assertEquals(counter, 2);
+    }
+}
