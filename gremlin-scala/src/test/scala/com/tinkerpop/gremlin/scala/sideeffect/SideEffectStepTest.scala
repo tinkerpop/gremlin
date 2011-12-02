d687c2e4f85e48e287250acf087007be967436ee
Zach Cox <zcox522@gmail.com>
2011-12-02 17:04:58 -0600

    asserts

--- gremlin-scala/src/test/scala/com/tinkerpop/gremlin/scala/sideeffect/SideEffectStepTest.scala
+++ gremlin-scala/src/test/scala/com/tinkerpop/gremlin/scala/sideeffect/SideEffectStepTest.scala
@@ -5,6 +5,7 @@ import com.tinkerpop.gremlin.test.ComplianceTest
 import com.tinkerpop.blueprints.pgm.Vertex
 import com.tinkerpop.gremlin.scala._
 import com.tinkerpop.pipes.Pipe
+import junit.framework.Assert._
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
@@ -19,17 +20,17 @@ class SideEffectStepTest extends com.tinkerpop.gremlin.test.sideeffect.SideEffec
     }
 
     def test_g_v1_sideEffectXstore_aX_propertyXnameX() {
-        var a = g.getVertex(2);
-        super.test_g_v1_sideEffectXstore_aX_propertyXnameX(g.v(1).->.sideEffect {v:Vertex => a = v}.property("name").asInstanceOf[Pipe[Vertex,String]]);
-        System.out.println(a + " must equal " + g.v(1));
-        //assertEquals(a, g.v(1))
+        var a = g.getVertex(2)
+        super.test_g_v1_sideEffectXstore_aX_propertyXnameX(g.v(1).->.sideEffect {v:Vertex => a = v}.property("name").asInstanceOf[Pipe[Vertex,String]])
+        System.out.println(a + " must equal " + g.v(1))
+        assertEquals(a, g.v(1))
     }
 
     def test_g_v1_out_sideEffectXincr_cX_propertyXnameX() {
         var c = 0;
         super.test_g_v1_out_sideEffectXincr_cX_propertyXnameX(g.v(1).->.out.sideEffect {v:Vertex => c=c+1}.property("name").asInstanceOf[Pipe[Vertex,String]]);
         System.out.println(c + " must equal 3");
-        //assertEquals(c, 3);
+        assertEquals(c, 3);
     }
 
     def test_g_v1_out_sideEffectXfalseX_propertyXnameX() {
