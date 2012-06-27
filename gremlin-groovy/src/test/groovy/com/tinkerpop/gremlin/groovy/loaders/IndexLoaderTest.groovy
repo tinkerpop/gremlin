0b88a7040b62423c48a5adbfe416ec02408f365c
Marko Rodriguez <okrammarko@gmail.com>
2012-06-27 11:45:28 -0600

    updated shell script to work with CYGWIN and a tweak to the index loader test cases in Gremlin-groovy.

--- gremlin-groovy/src/test/groovy/com/tinkerpop/gremlin/groovy/loaders/IndexLoaderTest.groovy
+++ gremlin-groovy/src/test/groovy/com/tinkerpop/gremlin/groovy/loaders/IndexLoaderTest.groovy
@@ -1,6 +1,8 @@
 package com.tinkerpop.gremlin.groovy.loaders
 
+import com.tinkerpop.blueprints.Edge
 import com.tinkerpop.blueprints.Graph
+import com.tinkerpop.blueprints.Vertex
 import com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory
 import com.tinkerpop.gremlin.groovy.Gremlin
 import junit.framework.TestCase
@@ -13,6 +15,8 @@ class IndexLoaderTest extends TestCase {
     public void testIndexShortcuts() throws Exception {
         Gremlin.load();
         Graph g = TinkerGraphFactory.createTinkerGraph();
+        g.createKeyIndex('name', Vertex.class);
+        g.createKeyIndex('weight', Edge.class);
         assertEquals(g.v(1), g.V('name', 'marko').next());
         assertEquals(2, g.E('weight', 0.4f).count());
     }
@@ -20,6 +24,7 @@ class IndexLoaderTest extends TestCase {
     public void testIndexHitIntoTraversal() throws Exception {
         Gremlin.load();
         Graph g = TinkerGraphFactory.createTinkerGraph();
+        g.createKeyIndex('lang', Vertex.class);
 
         def results = [];
         g.V('lang', 'java').inE.outV.fill(results);
