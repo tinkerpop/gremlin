60c4593c7acf24bddabb5d2bd503b528850c5781
Marko Rodriguez <okrammarko@gmail.com>
2012-11-04 10:21:59 -0700

    thread safe tests in GremlinGRoovyScriptEngine.

--- gremlin-groovy/src/test/java/com/tinkerpop/gremlin/groovy/jsr223/GremlinGroovyScriptEngineTest.java
+++ gremlin-groovy/src/test/java/com/tinkerpop/gremlin/groovy/jsr223/GremlinGroovyScriptEngineTest.java
@@ -5,8 +5,13 @@ import com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory;
 import junit.framework.TestCase;
 
 import javax.script.ScriptEngine;
+import javax.script.ScriptException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
@@ -33,6 +38,38 @@ public class GremlinGroovyScriptEngineTest extends TestCase {
         assertTrue(engine.eval("g = TinkerGraphFactory.createTinkerGraph()") instanceof TinkerGraph);
         assertTrue(engine.get("g") instanceof TinkerGraph);
         assertEquals(engine.eval("g.v(1)"), TinkerGraphFactory.createTinkerGraph().getVertex(1));
+    }
+
+    public void testThreadSafety() throws Exception {
+        final ScriptEngine engine = new GremlinGroovyScriptEngine();
+        engine.put("g", TinkerGraphFactory.createTinkerGraph());
+        int runs = 100;
+        final CountDownLatch latch = new CountDownLatch(runs);
+        final List<String> names = Arrays.asList("marko", "peter", "josh", "vadas", "stephen", "pavel", "matthias");
+        final Random random = new Random();
+
+        for (int i = 0; i < runs; i++) {
+            new Thread() {
+                public void run() {
+                    String name = names.get(random.nextInt(names.size() - 1));
+                    try {
+                        // final String var = "pipe" TODO: the variable is shared across threads
+                        final String var = "a" + UUID.randomUUID().toString().replace("-","");
+                        final Object result = engine.eval(var + " = g.V('name','" + name + "'); if("+var+".hasNext()) { "+var+".count() } else { null }");
+                        //System.out.println(name);
+                        if (name.equals("stephen") || name.equals("pavel") || name.equals("matthias"))
+                            assertNull(result);
+                        else
+                            assertNotNull(result);
+                    } catch (ScriptException e) {
+                        //System.out.println(e);
+                        assertFalse(true);
+                    }
+                    latch.countDown();
+                }
+            }.start();
+        }
+        latch.await();
     }
 
 }
